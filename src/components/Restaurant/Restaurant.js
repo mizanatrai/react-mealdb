8695c946a893d9dda2120027c406066d62ecbb9c
Mizanur Rahman <mizanbd.eee@gmail.com>
2022-03-30 00:53:16 +0600

    order data loaded from local storage

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Meal from '../Meal/Meal';
 import OrderList from '../OrderList/OrderList';
 import './Restaurant.css';
@@ -27,12 +27,39 @@ const Restaurant = () => {
         Read carefully, give it a try. [ Ki ache jibone]
         if  you need help, let us know in the support session
     */
+
+    useEffect(() => {
+        const storedOrder = getStoredCart();
+        const savedOrder = [];
+
+        for (const id in storedOrder) {
+            const addedMeal = meals.find(meal => meal.idMeal === id);
+            if (addedMeal) {
+                const quantity = storedOrder[id];
+                addedMeal.quantity = quantity;
+                savedOrder.push(addedMeal);
+            }
+        }
+        setOrders(savedOrder);
+    }, [meals])
+
     const handleAdToOrder = (meal) => {
-        console.log(meal);
+        let newOrders = [];
+        const exists = orders.find(m => m.idMeal === meal.idMeal);
+        if (exists) {
+            const rest = orders.filter(m => m.idMeal !== meal.idMeal);
+            exists.quantity = exists.quantity + 1;
+            newOrders = [...rest, exists]
+        }
+        else {
+            meal.quantity = 1;
+            newOrders = [...orders, meal];
+        }
         addToDb(meal.idMeal)
-        const newOrders = [...orders, meal];
         setOrders(newOrders);
     }
+
+
 
     return (
         <div className="restaurant-menu">
